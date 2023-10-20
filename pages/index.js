74a797e94da055bfb995c607409eb6efc84c131a
warrant <warrant1997@gmail.com>
2023-10-20 18:37:39 +1100

    update: index as home page

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,138 @@
+// modules
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { Button } from "@mui/material";
+// css
+// components
+import Layout from "./layout";
+// styled component
+import { VerticalButtons } from "../components/styled-component/Button";
+import { BrownCenterTitle } from "../components/styled-component/Title";
 
-export default function Home() {
+export default function Index() {
+  const router = useRouter();
+  const handleClick = (e, url) => {
+    e.preventDefault();
+    router.push(url);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Layout>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main sylte={{ paddingInline: "20vw" }}>
+          <BrownCenterTitle data-testid="home-title">
+            Greetings! <br />
+            Welcome to Warrant's website!
+          </BrownCenterTitle>
+          <VerticalButtons>
+            <Button
+              variant="contained"
+              data-testid="about-me"
+              sx={{
+                ":hover": {
+                  bgcolor: "#D7F72A",
+                  color: "black",
+                },
+                width: "300px",
+              }}
+              onClick={(e) => handleClick(e, "aboutme")}
+            >
+              About Me
+            </Button>
+            <Button
+              variant="contained"
+              data-testid="learning-path"
+              sx={{
+                ":hover": {
+                  bgcolor: "#D7F72A",
+                  color: "black",
+                },
+                width: "300px",
+              }}
+              onClick={(e) => handleClick(e, "learningpath")}
+            >
+              Learning Path
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              data-testid="learning-path"
+              sx={{
+                ":hover": {
+                  bgcolor: "#D7F72A",
+                  color: "black",
+                },
+                width: "300px",
+              }}
+              onClick={(e) => handleClick(e, "mysideprojects")}
+            >
+              My Side Projects
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              data-testid="learning-path"
+              sx={{
+                ":hover": {
+                  bgcolor: "#D7F72A",
+                  color: "black",
+                },
+                width: "300px",
+              }}
+              onClick={(e) => handleClick(e, "mycodingbible")}
+            >
+              Coding Bible
+            </Button>
+            <Button
+              variant="contained"
+              data-testid="laboratory"
+              sx={{
+                ":hover": {
+                  bgcolor: "#D7F72A",
+                  color: "black",
+                },
+                width: "300px",
+              }}
+              onClick={(e) => handleClick(e, "lab")}
+            >
+              Laboratory
+            </Button>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+            <Button
+              variant="contained"
+              data-testid="my-hobbies"
+              sx={{
+                ":hover": {
+                  bgcolor: "#D7F72A",
+                  color: "black",
+                },
+                width: "300px",
+              }}
+              onClick={(e) => handleClick(e, "myhobbies")}
+            >
+              My Hobbies
+            </Button>
+            <Button
+              variant="contained"
+              data-testid="working-experience"
+              sx={{
+                ":hover": {
+                  bgcolor: "#D7F72A",
+                  color: "black",
+                },
+                width: "300px",
+              }}
+              onClick={(e) => handleClick(e, "workingexperience")}
+            >
+              Working Experience
+            </Button>
+          </VerticalButtons>
+        </main>
+      </Layout>
+    </>
   );
 }
