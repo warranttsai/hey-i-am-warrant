2546d7b11cad87cd74503c826cd0832ea5e0566c
warrant <warrant1997@gmail.com>
2023-10-20 18:31:18 +1100

    refacotr: footer and header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,46 +1,44 @@
 // modules
+import React from "react";
 import { Toolbar, AppBar, Button, Box } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 function Header() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <AppBar position="static" style={{ width: "100%" }}>
       <Toolbar sx={{ justifyContent: "flex-end" }}>
         <Button
           color="inherit"
-          onClick={() => navigate("home")}
+          // onClick={() => navigate("/")}
           sx={{ marginRight: "auto", fontSize: "25px", padding: "0" }}
         >
           🏡
         </Button>
-        <Box>
-          <Button color="inherit" onClick={() => navigate("about-me")}>
+        {/* <Box>
+          <Button color="inherit" onClick={() => navigate("aboutme")}>
             ABOUT ME
           </Button>
-          <Button color="inherit" onClick={() => navigate("learning-path")}>
+          <Button color="inherit" onClick={() => navigate("learningpath")}>
             LEARNING PATH
           </Button>{" "}
-          <Button color="inherit" onClick={() => navigate("my-side-projects")}>
+          <Button color="inherit" onClick={() => navigate("mysideprojects")}>
             My SIDE PROJECTS
           </Button>
-          <Button color="inherit" onClick={() => navigate("/my-coding-bible")}>
+          <Button color="inherit" onClick={() => navigate("/mycodingbible")}>
             Coding Bible
           </Button>
           <Button color="inherit" onClick={() => navigate("lab")}>
             Laboratory
           </Button>
-          <Button color="inherit" onClick={() => navigate("my-hobbies")}>
+          <Button color="inherit" onClick={() => navigate("myhobbies")}>
             MY HOBBIES
           </Button>
-          <Button
-            color="inherit"
-            onClick={() => navigate("working-experience")}
-          >
+          <Button color="inherit" onClick={() => navigate("workingexperience")}>
             WORKING EXPERIENCE
           </Button>
-        </Box>
+        </Box> */}
       </Toolbar>
     </AppBar>
   );
