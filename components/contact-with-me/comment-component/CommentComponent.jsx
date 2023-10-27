f198026ecce7168c3451d4528ced3a9d106a8497
warrant <warrant1997@gmail.com>
2023-10-28 10:35:22 +1100

    fix: wrong global style of flex

--- components/contact-with-me/comment-component/CommentComponent.jsx
+++ components/contact-with-me/comment-component/CommentComponent.jsx
@@ -26,57 +26,61 @@ export default function CommentComponent({ index, comment }) {
               layout="responsive"
               alt="Picture of the commentor"
             />
-            <SpeechBubbleLeft className="flex-2 text-left w-100">
-              <span
-                className="flex-1 w-100"
-                style={{ fontWeight: 900, fontSize: 20 }}
-              >
-                {comment.user_name !== "" ? comment.user_name : "<unknown>"},{" "}
-                {comment.comment_date !== ""
-                  ? comment.comment_date
-                  : "<no date>"}
-                ,{" "}
-                {comment.comment_time !== ""
-                  ? comment.comment_time
-                  : "<no time>"}
-              </span>
-              <span
-                style={{
-                  display: "block",
-                  width: "100%",
-                  wordWrap: "break-word",
-                }}
-              >
-                {comment.comment !== "" ? comment.comment : "<no comment>"}
-              </span>
-            </SpeechBubbleLeft>
+            <div className="flex-2 text-left w-100">
+              <SpeechBubbleLeft>
+                <span
+                  className="flex-1 w-100"
+                  style={{ fontWeight: 900, fontSize: 20 }}
+                >
+                  {comment.user_name !== "" ? comment.user_name : "<unknown>"},{" "}
+                  {comment.comment_date !== ""
+                    ? comment.comment_date
+                    : "<no date>"}
+                  ,{" "}
+                  {comment.comment_time !== ""
+                    ? comment.comment_time
+                    : "<no time>"}
+                </span>
+                <span
+                  style={{
+                    display: "block",
+                    width: "100%",
+                    wordWrap: "break-word",
+                  }}
+                >
+                  {comment.comment !== "" ? comment.comment : "<no comment>"}
+                </span>
+              </SpeechBubbleLeft>
+            </div>
           </>
         ) : (
           <>
-            <SpeechBubbleRight className="flex-2 d-flex flex-column text-left">
-              <span
-                className="flex-1"
-                style={{ fontWeight: 900, fontSize: 20 }}
-              >
-                {comment.user_name !== "" ? comment.user_name : "<unknown>"},{" "}
-                {comment.comment_date !== ""
-                  ? comment.comment_date
-                  : "<no date>"}
-                ,{" "}
-                {comment.comment_time !== ""
-                  ? comment.comment_time
-                  : "<no time>"}
-              </span>
-              <span
-                style={{
-                  display: "block",
-                  width: "100%",
-                  wordWrap: "break-word",
-                }}
-              >
-                {comment.comment !== "" ? comment.comment : "<no comment>"}
-              </span>
-            </SpeechBubbleRight>
+            <div className="flex-2 text-left w-100">
+              <SpeechBubbleRight className="flex-2 d-flex flex-column text-left">
+                <span
+                  className="flex-1"
+                  style={{ fontWeight: 900, fontSize: 20 }}
+                >
+                  {comment.user_name !== "" ? comment.user_name : "<unknown>"},{" "}
+                  {comment.comment_date !== ""
+                    ? comment.comment_date
+                    : "<no date>"}
+                  ,{" "}
+                  {comment.comment_time !== ""
+                    ? comment.comment_time
+                    : "<no time>"}
+                </span>
+                <span
+                  style={{
+                    display: "block",
+                    width: "100%",
+                    wordWrap: "break-word",
+                  }}
+                >
+                  {comment.comment !== "" ? comment.comment : "<no comment>"}
+                </span>
+              </SpeechBubbleRight>
+            </div>
             <Image
               src={ProfilePicPlaceholder}
               className="flex-1 w-100 h-100"
